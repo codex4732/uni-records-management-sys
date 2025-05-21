259a9d0fdab3e4fc20a9fe915008b27bc94b2e91
Julian Chetty <jujudufrene@gmail.com>
2025-05-21 21:24:04 +0400

    remove connectionstrings

--- app/config.py
+++ app/config.py
@@ -18,27 +18,13 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DEV_DATABASE_URL',
-        'mysql+pymysql://root:dev_password@localhost/urms_dev'
     )
 
 class ProductionConfig(Config):
     """Production configuration"""
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'mysql+pymysql://prod_user:prod_password@localhost/urms_prod'
+        'DATABASE_URL'
     )
 
-class TestConfig(Config):
-    """Test configuration"""
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'test-key'
-
-config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'testing': TestConfig,
-    'default': DevelopmentConfig
-}
+config = dict(development=DevelopmentConfig, production=ProductionConfig, default=DevelopmentConfig)
